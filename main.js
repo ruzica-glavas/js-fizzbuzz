b702eec0b5cf28dcdc988dbd32bfa58407ef1923
ruzica-glavas <ruzi99@hotmail.it>
2025-01-13 09:48:17 +0100

    add: correct "else"

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ for ( let i=1; i<=100; i++){
         console.log("Buzz")
 
     } else{
-        console.log(`Il tuo numero ${i} non é divisibile né per 3 né per 5`)
+        console.log(`Il tuo numero ${i} non é divisibile né per 3 né per 5: ${i}`)
     }
 
 }
